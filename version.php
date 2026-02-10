79bf8222e5860926730698ab4e856930072cd7b3
Lai Wei <lai.wei@enovation.ie>
2026-02-10 09:54:24 +0000

    Update plugin versions for Moodle 5.1.0 release

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025040805;
-$plugin->requires = 2025040800;
-$plugin->release = '5.0.4';
+$plugin->version = 2025100600;
+$plugin->requires = 2025100600;
+$plugin->release = '5.1.0';
 $plugin->component = 'assignfeedback_onenote';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'local_onenote' => 2025040805,
+    'local_onenote' => 2025100600,
 ];
